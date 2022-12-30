a0afa1590b639e095598cf6a078b47d928100228
jibundeyare <contact@jibundeyare.com>
2022-12-30 13:24:04 +0100

    Création d'un back office
    Création d'un fromulaire d'authentification.
    Les utilisateurs sont renvoyés à des pages différentes en fonction de
    leur rôle.
    
    Création d'un CRUD pour les profils emprunteurs.
    L'admin a accès à toutes les routes.
    Les emprunteurs ne peuvent pas créer de nouveau profile emprunteur.
    Les emprunteurs ne peuvent pas supprimer de profile emprunteur existant.
    Les emprunteurs ne peuvent consulter que leur profile personnel.
    
    TODO :
    
    - Corriger le formulaire EmprunteurType
    - Les emprunteurs ne peuvent lister que leurs profiles personnels.
    - Les emprunteurs ne peuvent modifier que leur profile personnel.

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\EmprunteurRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    private ?EmprunteurRepository $emprunteurRepository = null; 
+
+    public function __construct(private UrlGeneratorInterface $urlGenerator, EmprunteurRepository $emprunteurRepository)
+    {
+        $this->emprunteurRepository = $emprunteurRepository;
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email', '');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($request->request->get('password', '')),
+            [
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        $user = $token->getUser();
+        $roles = $user->getRoles();
+
+        if (in_array('ROLE_ADMIN', $roles)) {
+            // l'utilisateur est un admin
+            return new RedirectResponse($this->urlGenerator->generate('app_emprunteur_index'));
+        } elseif (in_array('ROLE_EMPRUNTEUR', $roles)) {
+            // l'utilisateur est un emprunteur
+            $emprunteur = $this->emprunteurRepository->findByUser($user);
+
+            return new RedirectResponse($this->urlGenerator->generate('app_emprunteur_show', [
+                'id' => $emprunteur->getId(),
+            ]));
+        }
+
+        // En dernier lieu, l'utilisateur est renvoyé vers la page d'accueil
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
